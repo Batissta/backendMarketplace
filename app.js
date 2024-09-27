1b8a7503962206e7a92b325235f69c4241707fa3
Francinaldo Batista <140225853+Batissta@users.noreply.github.com>
2024-09-26 23:12:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const clientRoute = require("./src/routes/clienteRoute");
 const productRoute = require("./src/routes/produtoRoute");
 const userRoute = require("./src/routes/usuarioRoute");
 
+app.use(cors());
 app.use(express.json());
 app.use("/clientes", clientRoute);
 app.use("/produtos", productRoute);
